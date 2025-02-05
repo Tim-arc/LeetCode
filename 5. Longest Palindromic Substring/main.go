59f4346e9d43c6abf8e67ee43855d9ec47f422a2
Tim <timakos179@gmail.com>
2025-02-05 17:04:40 +0300

    Solve 5

--- 5. Longest Palindromic Substring/main.go
+++ 5. Longest Palindromic Substring/main.go
@@ -1,13 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+func expandAroundCenter(s string, left int, right int) string {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return s[left+1 : right]
+}
+
 func longestPalindrome(s string) string {
-    
+	if len(s) == 0 {
+		return ""
+	}
+
+	longest := ""
+	for i := 0; i < len(s); i++ {
+		// Обработка нечетных палиндромов
+		palindrome1 := expandAroundCenter(s, i, i)
+		// Обработка четных палиндромов
+		palindrome2 := expandAroundCenter(s, i, i+1)
+
+		// Выбираем самый длинный палиндром
+		if len(palindrome1) > len(longest) {
+			longest = palindrome1
+		}
+		if len(palindrome2) > len(longest) {
+			longest = palindrome2
+		}
+	}
+	return longest
 }
 
 func main() {
-	
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	s := ""
+	fmt.Fscan(in, &s)
+	fmt.Fprintln(out, longestPalindrome(s))
 }
